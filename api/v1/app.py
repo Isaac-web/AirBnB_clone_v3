476ce7ccbdc402de1720e5bc9b8ffa2741ac33bf
Isaac Takiy <kanytakiy@gmail.com>
2022-11-20 12:29:49 +0000

    Modify app.py and views/index.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 Contains a flask app
 """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.view import app_views
 import os
@@ -15,6 +15,14 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def close_storage():
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found():
+    """
+    Not found response
+    """
+    return make_response(jsonify({"error": "Not found"})), 404
 
 
 if __name__ == "__main__":
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,27 @@
 
 from api.v1.views import app_view
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_view.route("/status")
 def status():
     """Returns the status of the app"""
     return jsonify({"status": "OK"})
+
+
+@app_view.route("/stats")
+def get_stats():
+    """Statistics of application data"""
+    return (jsonify({"amenities": storage.count(Amenity),
+                     "cities": storage.count(City),
+                     "places": storage.count(Place),
+                     "reviews": storage.count(Review),
+                     "states": storage.count(State),
+                     "users": storage.count(User)}))

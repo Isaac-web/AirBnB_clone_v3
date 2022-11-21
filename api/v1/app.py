7251ee3f731ac696afc1d5753cec8b0e1c71f748
Isaac-web <kanytakiy@gmail.com>
2022-11-21 05:02:01 +0000

    Testing...

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
-"""
-Contains a flask app
-"""
-from flask import Flask
+"""flask"""
+from flask import Flask, render_template, jsonify
 from models import storage
 from api.v1.views import app_views
-import os
+from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix="/api/v1")
+host = getenv('HBNB_API_HOST') if getenv('HBNB_API_HOST') else '0.0.0.0'
+port = getenv('HBNB_API_PORT') if getenv('HBNB_API_PORT') else 5000
+cors = CORS(app, resources={'/*': {'origins': '0.0.0.0'}})
 
 
 @app.teardown_appcontext
-def tear_down(self):
+def teardown_db(self):
+    """teardown"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host=os.getenv("HBNB_API_HOST", "0.0.0.0"),
-            port=os.getenv("HBNB_API_PORT", 5000), threaded=True)
+@app.errorhandler(404)
+def page_not_found(e):
+    """404ed"""
+    return jsonify({"error": "Not found"}), 404
+
+if __name__ == '__main__':
+    app.run(host=host, port=port, threaded=True)
